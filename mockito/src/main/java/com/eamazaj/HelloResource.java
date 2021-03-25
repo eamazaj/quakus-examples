c5079e11d47dd969508e593a00da054eaa08c7bd
Edwin Maza Jara <eamazaj@gmail.com>
2021-03-25 12:18:05 -0500

    feat: add new resource

--- mockito/src/main/java/com/eamazaj/HelloResource.java
+++ mockito/src/main/java/com/eamazaj/HelloResource.java
@@ -1,10 +1,14 @@
 package com.eamazaj;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.HashMap;
+import java.util.Map;
 
 @Path("/hello")
 public class HelloResource {
@@ -12,9 +16,21 @@ public class HelloResource {
     @Inject
     HelloService helloService;
 
+    @ConfigProperty(name = "username")
+    String username;
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return helloService.sayHello();
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/secrets")
+    public Map<String, String> getSecrets() {
+        Map<String, String> map = new HashMap<>();
+        map.put("username", username);
+        return map;
     }
 }
